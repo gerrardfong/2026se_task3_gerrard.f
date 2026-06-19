d773c4ba9744601fa34cc68ff731d84ca2abe76c
Gerrard Fong <gerrard.fong@education.nsw.gov.au>
2026-06-19 03:35:38 +0000

    Feature WIP: Working on structure for gauntlet.py

---
+++ 2. PWA/pyscripts/routes/gauntlet_generation.py
@@ -0,0 +1,22 @@
+import sqlite3
+import random
+from flask import session
+from routes import enemy_generation as dbChar
+
+
+# Gamemode
+def endless():
+    pass
+
+# Gamemode
+def waves():
+    pass
+
+# Creates enemy for waves and endless
+def generate_enemy():
+    pass
+
+# Adds xp from enemy to player when a character defeats the enemy
+def add_xp():
+    pass
+
